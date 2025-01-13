4a8fdac9d2c46cd997b5c06d36f61137acc74941
Benjamin M. D. Fahl <befahl@ethz.ch>
2025-01-13 10:32:22 +0100

    added main.py entry point

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from dynmat import UnitCell
+
+VERBOSE = True
+
+
+def main():
+    START_MSG = "Starting dynmat..."
+    print(START_MSG) if VERBOSE else None
+
+
+if __name__ == "__main__":
+    main()
